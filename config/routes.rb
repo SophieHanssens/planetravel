6b5736e1266c4d185f6afa7519e9806d887025fa
Loris Reynaud <loris.reynaud@gmail.com>
2021-12-14 11:23:13 +0100

    routes ok

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,15 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  ressources :planets do
-    ressources :bookings, only: [:new, :create]
-    patch '/bookiongs/:id', to: 'bookings#accept'
-    patch '/bookiongs/:id', to: 'bookings#reject'
+  resources :planets do
+    resources :bookings, only: [:new, :create]
   end
-  ressources :bookings, only: :destroy
+  resources :bookings, only: :destroy do
+    member do
+      patch 'accept'
+      patch 'reject'
+    end
+  end
   get '/', to: 'pages#home'
   get '/about', to: 'pages#about'
 end
