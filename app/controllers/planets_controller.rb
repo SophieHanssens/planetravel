c4afd39e1659973fe31e0ab19f5914231ec7362b
Loris Reynaud <loris.reynaud@gmail.com>
2021-12-14 15:25:17 +0100

    adding planet ok

--- app/controllers/planets_controller.rb
+++ app/controllers/planets_controller.rb
@@ -3,4 +3,29 @@ class PlanetsController < ApplicationController
     @planets = Planet.all
   end
 
+  def new
+    @planet = Planet.new
+  end
+
+  def create
+    @planet = Planet.new(planet_params)
+    @user = current_user.id
+    @planet.user_id = @user
+    if @planet.save!
+      redirect_to planet_path(@planet)
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @planet = Planet.find(params[:id])
+  end
+
+  private
+
+  def planet_params
+    params.require(:planet).permit(:name, :address, :available_places, :price_per_night, :user_id)
+  end
+
 end
