ba07a36bdd2afdd7b036df8d8028b061f4facbe4
Loris Reynaud <loris.reynaud@gmail.com>
2021-12-14 11:06:08 +0100

    routes created

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  ressources :planets do
+    ressources :bookings, only: [:new, :create]
+    patch '/bookiongs/:id', to: 'bookings#accept'
+    patch '/bookiongs/:id', to: 'bookings#reject'
+  end
+  ressources :bookings, only: :destroy
+  get '/', to: 'pages#home'
+  get '/about', to: 'pages#about'
 end
