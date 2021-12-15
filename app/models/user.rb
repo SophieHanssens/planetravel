7116c870d059012a2cce9b6f5693b1af449c37b7
Sophie Hanssens <sophie.hanssens@gmail.com>
2021-12-15 16:46:44 +0100

    zut

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :planets, dependent: :destroy
   has_many :bookings, through: :planets, dependent: :destroy
+  has_many :planets, dependent: :destroy
 
   validates :name, presence: true, uniqueness: true
   validates :phone_number, presence: true, numericality: true, length: { minimum: 8, maximum: 14}
